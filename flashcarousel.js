706551cc52fed0432437881137b831124a6f5904
Flash <me@flashycud.com>
2015-07-21 11:27:01 -0700

    complete version 0.0.1

--- flashcarousel.js
+++ flashcarousel.js
@@ -14,29 +14,112 @@
 })(this, function () {
 
   var FlashCarousel,
-      _options;
 
-  _options = {
-    
+      Instance,
+      attachNav,
+      attachPage,
+
+      _o;
+
+  _o = {
+    zIndexBase: 0
   }
 
   FlashCarousel = function (t, options) {
     t = $(t);
-    $.extend(_options, options);
+    new Instance(t, options);
+  };
 
-    var el = t.children(),
-        holder; 
 
-    t.addClass('flash-carousel');
+  Instance = function(t, options) {
+    var wrapper, el, holder,
+        _this = this;
+    
+    this.target = t;
+    this.options = {};
+    this.items = [];
+    this.pos = [];
+    this.curr = 0;
+
+    $.extend(this.options, _o, options);
+    
+    this.target.addClass('flash-carousel');
+    wrapper = $('<div class="flash-carousel-slides">');
+    el = this.target.children();
+
     el.each(function (i) {
       var $this = $(this);
-      
       $this.detach();
       holder = $('<div class="flash-carousel-slide">')
       holder.append($this);
-      t.append(holder);
-    })
-  };
+      wrapper.append(holder);
+      _this.pos.push(_this.target.width() * i);
+      holder.css("left", _this.pos[i]);
+      _this.items.push(holder);
+    });
+
+    this.target.append(wrapper);
+
+    attachNav.call(this);    
+    attachPage.call(this);    
+
+  }
+
+  Instance.prototype.navigateTo = function(page) {
+    for (var i=0, l=this.items.length; i<l; i++) {
+      this.pages[this.curr].removeClass("active");
+      this.curr = (page < 0)? l-1: (page >= l)? 0: page;
+      this.pages[this.curr].addClass("active");
+      this.pos[i] = this.target.width() * (i - this.curr);
+      this.items[i].css("left", this.pos[i]);
+    }
+  }
+
+  attachNav = function() {
+    var left = $('<div class="flash-carousel-nav-left">'),
+        right = $('<div class="flash-carousel-nav-right">'),
+        _this = this;
+
+    left.css("z-index", this.options.zIndexBase+1)
+      .append($("<div class='flash-carousel-nav-left-arrow'>Previous</div>"))
+      .click(function () {
+        _this.navigateTo(_this.curr - 1);
+      });
+    right.css("z-index", this.options.zIndexBase+1)
+      .append($("<div class='flash-carousel-nav-right-arrow'>next</div>"))
+      .click(function () {
+        _this.navigateTo(_this.curr + 1);
+      });
+
+    this.target.append(left).append(right);
+  }
+
+  attachPage = function() {
+    var holder = $('<div class="flash-carousel-dots">'),
+        page,
+        _this = this;
+
+    this.pages = [];
+
+    for (var i=0, l=this.items.length; i<l; i++) {
+      page = $('<div class="flash-carousel-dot">');
+      if (i == 0) {
+        page.addClass('active');
+      }
+      this.pages.push(page);
+      holder.append(page);
+    }
+    $(this.pages).each(function (i) {
+      $(this).click(function () {
+        _this.navigateTo(i);
+      });
+    });
+
+    holder.css("z-index", this.options.zIndexBase+2);
+    
+    this.target.append(holder);
+  }
+
 
   return FlashCarousel;
 
